51dc9c8b9a053a26af4f4118d41966342a0a3a9f
git-marcopitra <marcomorete73@hotmail.com>
2023-12-11 11:31:46 +0100

    :bug: fix: hide on expend

--- components/chat/index.jsx
+++ components/chat/index.jsx
@@ -2,10 +2,15 @@ import { Box } from "@/elements";
 import StickyMessage from "./sticky-message";
 import { CHAT_MESSAGES, LONG_STICKY_MESSAGE } from "./chat.mock";
 import BallonMessage from "./ballon-message";
+import { useState } from "react";
 
 const Chat = () => {
-  const data = CHAT_MESSAGES.slice(-4) ;
+  const data = CHAT_MESSAGES.slice(-4);
   const stickyMessage = LONG_STICKY_MESSAGE;
+
+  const [isExpandedText, setExpandText] = useState(false);
+
+  const toggleExpandText = () => setExpandText((v) => !v);
 
   return (
     <Box color="white">
@@ -17,15 +22,23 @@ const Chat = () => {
         alignItems="flex-start"
         maxWidth="calc(100vw - 6rem)"
       >
-        {data.map(({ id, ...chatMessage }, index) => (
-          <BallonMessage
-            key={id}
-            {...chatMessage}
-            isLast={index === 0 && data.length === 4}
-          />
-        ))}
+        {!isExpandedText &&
+          stickyMessage.message.length > 48 &&
+          data.map(({ id, ...chatMessage }, index) => (
+            <BallonMessage
+              key={id}
+              {...chatMessage}
+              isLast={index === 0 && data.length === 4}
+            />
+          ))}
       </Box>
-      {stickyMessage && <StickyMessage stickyMessage={stickyMessage} />}
+      {stickyMessage && (
+        <StickyMessage
+          stickyMessage={stickyMessage}
+          isExpandedText={isExpandedText}
+          toggleExpandText={toggleExpandText}
+        />
+      )}
     </Box>
   );
 };
